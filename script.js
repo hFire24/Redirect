1f4408b68066d7133290c2fbfd6959a065477b90
cFreeze9 <51500563+cFreeze9@users.noreply.github.com>
2019-12-22 16:02:19 -0500

    Update script.js
    Changed a break message so that I don't spend too much time on one.

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function loadBreak() {
   'If it\'s the evening, change to your pajamas. No socks.',
   'Declutter your room.',
   'Walk around for about 1-2 minutes.',
-  'Go on a journey. A Kino\'s Journey if you wish.'];
+  'Drive for at least 20 minutes.'];
   //Get random message index from messages array
   var index = Math.floor(Math.random() * messages.length);
   if (index === 0)
