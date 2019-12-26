030587f3a8fb4b578ddb691391aafe870418bfe0
cFreeze9 <51500563+cFreeze9@users.noreply.github.com>
2019-12-26 00:02:17 -0500

    Updated script.js
    loadBreak() now has more break opitons; mainly those from randomLink().
    linkMessages only consist of two messages: Immediately close this tab., or Ready to do something else?.

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function loadWebsite(number) {
 
 function loadBreak() {
   var messages = ['Listen to this.',
+  'Check your gTasks list, please.',
+  'Check your repeating tasks on To-Do.',
+  'Check your anime queue spreadsheet.',
   'Pray a short prayer.',
   'Look away from the screen for about 30 seconds.',
   'Compliment someone you know online.',
@@ -37,19 +40,23 @@ function loadBreak() {
   'Declutter your room.',
   'Walk around for about 1-2 minutes.',
   'Drive for at least 20 minutes.'];
+  for (var i = 0; i < 6; i++)
+    linkMessages.push("Watch anime.");
   //Get random message index from messages array
   var index = Math.floor(Math.random() * messages.length);
   if (index === 0)
     return "<a href=\"https://open.spotify.com/album/7Cff6vcc5DQ51FZ0DPLqXD\">" + messages[index] + "</a>";
-  if (index === 4)
+  if (index === 7)
     return "<a href=\"https://www.google.com/search?q=breathing+exercise\">" + messages[index] + "</a>";
   if (index === messages.indexOf("Learn how to make trance music."))
     return "<a href=\"trancelessons.html\">" + messages[index] + "</a>";
   if (index === messages.indexOf("Learn how to make trance music.") + 1)
     return "<a href=\"https://emergency.nofap.com/redirect?religious=true&cat=em\">" + messages[index] + "</a>";
-  if (index >= messages.indexOf("Walk around for about 1-2 minutes."))
+  if (index >= messages.indexOf("Watch anime."))
+    return "<a href=\"nextanime.html\">" + linkMessages[index] + "</a>";
+  else if (index >= messages.indexOf("Walk around for about 1-2 minutes."))
     return "Get your laptop off your lap, and stand up.<br>Change to appropriate clothes if necessary.<br>" + messages[index];
-  if (index >= messages.indexOf("Refill your bottle and drink water."))
+  else if (index >= messages.indexOf("Refill your bottle and drink water."))
     return "Get your laptop off your lap, and stand up.<br>" + messages[index];
   else
     return messages[index];
@@ -85,39 +92,15 @@ function randomMessage() {
 }
 
 function randomLink(message) {
-  /*The linkMessages are, in order
-  A command to close this tab immediately.
-  A prompt to check my gTasks list.
-  A prompt to check my repeating tasks.
-  A prompt to check my anime queue spreadsheet.
-  A link to the nextanime page containing links to episodes.
-  A randomly generated break message.*/
-  var linkMessages = ["Immediately close this tab.",
-  "Check your gTasks list, please.",
-  "Check your repeating tasks on To-Do.",
-  "Check your anime queue spreadsheet."];
-  for (var i = 4; i <= 9; i++)
-    linkMessages.push("Watch anime.");
+  var linkMessages = ["Immediately close this tab."];
   //These variables are needed so that the loop doesn't become an infinite loop.
-  var onehundredten = linkMessages.length + 1;
-  var onehundredeleven = linkMessages.length * 3;
-  for (i = onehundredten; i <= onehundredeleven; i++)
-    linkMessages.push(loadBreak());
+  for (i = 1; i < 30; i++)
+    linkMessages.push("Ready to do something else?");
   //Get random link from array
   var index = Math.floor(Math.random() * linkMessages.length);
-  //For getting back on task
-  if (message > 5 && message < 8)
-    index = Math.floor(Math.random() * linkMessages.lastIndexOf("Watch anime.") + 1);
-  //For taking breaks
-  var breaktime = linkMessages.lastIndexOf("Watch anime.") + 1;
-  if (message === 11)
-      index = Math.floor(Math.random() * (linkMessages.length - breaktime)) + breaktime;
-  //For checking tasks
-  if (message === 12)
-    index = Math.floor(Math.random() * 2) + 1;
   //Put link message and to screen using the index value of the array
-  if (index >= 4 && index <= 9)
-    document.getElementById("link").innerHTML = "<a href=\"nextanime.html\">" + linkMessages[index] + "</a>";
+  if (index > 0)
+    document.getElementById("link").innerHTML = "<a href=\"breaktime.html\">" + linkMessages[index] + "</a>";
   else
     document.getElementById("link").innerHTML = linkMessages[index];
   //Put index value to the console
@@ -200,20 +183,18 @@ function getTitle(id) {
 
 function loadAnime() {
   var anime = [];
-  anime.push(new Anime(getTitle("138-2") + "Episode 25","https://vrv.co/watch/GYJQ27336/Hayate-the-Combat-Butler-Season-1-2:Its-really-a-story-about-a-young-lady-and-her-butler"));
-  anime.push(new Anime(getTitle("153") + "Episode 10","https://vrv.co/watch/GY491EJ0R/Plastic-Memories:No-Longer-Partners"));
+  anime.push(new Anime(getTitle("24") + "Episode 23","https://www.funimation.com/shows/pani-poni-dash/misfortunes-never-come-one-by-one"));
   anime.push(new Anime(getTitle("145-2") + "Episode 11","https://vrv.co/watch/GY3VKMWZR/Saki:Hand-11-Threat"));
   anime.push(new Anime(getTitle("149") + "Episode 24","https://vrv.co/watch/GY4PXWX16/ReZERO-Starting-Life-in-Another-World-:The-Self-Proclaimed-Knight-and-the-Greatest-Knight"));
-  anime.push(new Anime(getTitle("24") + "Episode 23","https://www.funimation.com/shows/pani-poni-dash/misfortunes-never-come-one-by-one"));
   anime.push(new Anime(getTitle("153") + "Episode 11","https://vrv.co/watch/GYJQVX336/Plastic-Memories:Rice-Omelette-Day"));
+  anime.push(new Anime(getTitle("24") + "Episode 24","https://www.funimation.com/shows/pani-poni-dash/you-are-responsible-for-your-own-death"));
   anime.push(new Anime(getTitle("145-2") + "Episode 12","https://vrv.co/watch/G62P4E596/Saki:Hand-12-Truth"));
   anime.push(new Anime(getTitle("149") + "Episode 25","https://vrv.co/watch/GYJ0M4M26/ReZERO-Starting-Life-in-Another-World-:Thats-All-This-Story-Is-About"));
-  anime.push(new Anime(getTitle("24") + "Episode 24","https://www.funimation.com/shows/pani-poni-dash/you-are-responsible-for-your-own-death"));
-  anime.push(new Anime(getTitle("153") + "Episode 12","https://vrv.co/watch/GYK534M8R/Plastic-Memories:Filling-Up-with-Memories"));
-  anime.push(new Anime(getTitle("145") + "Episode 13","https://vrv.co/watch/G6ZXJD4MR/Saki:Hand-13-Old-Friends"));
   anime.push(new Anime(getTitle("24") + "Episode 25","https://www.funimation.com/shows/pani-poni-dash/a-time-of-crisis"));
+  anime.push(new Anime(getTitle("145-2") + "Episode 13","https://vrv.co/watch/G6ZXJD4MR/Saki:Hand-13-Old-Friends"));
+  anime.push(new Anime(getTitle("153") + "Episode 12","https://vrv.co/watch/GYK534M8R/Plastic-Memories:Filling-Up-with-Memories"));
+  anime.push(new Anime(getTitle("24") + "Episode 26","https://www.funimation.com/shows/pani-poni-dash/no-one-knows-what-may-happen-tomorrow"));
   anime.push(new Anime(getTitle("153") + "Episode 13","https://vrv.co/watch/G6VN35ZPR/Plastic-Memories:I-Hope-One-Day-Youll-be-Reunited"));
-  anime.push(new Anime(getTitle("24") + "Episode 26","https://www.funimation.com/shows/pani-poni-dash/no-one-knows-what-may-happen-tomorrow"));  anime.push(new Anime(getTitle("145-2") + "Episode 13","https://vrv.co/watch/G6ZXJD4MR/Saki:Hand-13-Old-Friends"));
   anime.push(new Anime(getTitle("35") + "Episode 1","https://vrv.co/watch/GR8VMM18R/GJ-CLUB:I-am-the-GJ-Club"));
   anime.push(new Anime(getTitle("35") + "Episode 2","https://vrv.co/watch/G6DQVVDGR/GJ-CLUB:Friendship-Love-Her-Abnormality"));
   var x = document.getElementById("animelist");
