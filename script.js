d67325d9ab03aa588f05e0b893105977845b3808
cFreeze9 <51500563+cFreeze9@users.noreply.github.com>
2019-12-28 23:17:47 -0500

    Changed a lot
    Watch anime is now in the middle of the array.
    Replaced as many document.getElementById lines as possible

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var number = 0;
 
 function loadWebsite(number) {
   if (number === 2)
-    document.getElementById("message").innerHTML = loadBreak();
+    loadBreak();
   else if (number === 4)
     document.getElementById("message").innerHTML = "☢THAT UNSAFE WEBSITE IS BLOCKED!☢";
   else
@@ -39,6 +39,12 @@ function loadBreak() {
   'Try not to fap for 30 days straight, even if you stream all kinds of anime.',
   'How can you build an everlasting romantic relationship from scratch?',
   'Think of a goal. Any one. Set that as your break message.',
+  'Watch anime.',
+  'Watch anime.',
+  'Watch anime.',
+  'Watch anime.',
+  'Watch anime.',
+  'Watch anime.',
   'Refill your bottle and drink water.',
   'Go up and down the stairs.',
   'Stare at an object 20 feet away for 20 seconds.',
@@ -51,28 +57,27 @@ function loadBreak() {
   'Declutter your room.',
   'Walk around for about 1-2 minutes.',
   'Drive for as long as you want to get your mind off of things.'];
-  for (var i = 0; i < 6; i++)
-    messages.push("Watch anime.");
   //Get random message index from messages array
   var index = Math.floor(Math.random() * messages.length);
+  var message = document.getElementById("message");
   if (index === 0)
-    return "<a href=\"https://open.spotify.com/album/7Cff6vcc5DQ51FZ0DPLqXD\">" + messages[index] + "</a>";
+    message.href = "https://open.spotify.com/album/7Cff6vcc5DQ51FZ0DPLqXD";
   if (index === messages.indexOf("Look away from the screen for about 30 seconds."))
-    return "<a href=\"https://www.google.com/search?q=30+second+timer\">" + messages[index] + "</a>";
+    message.href = "https://www.google.com/search?q=30+second+timer";
   if (index === messages.indexOf("Take a deep breath."))
-    return "<a href=\"https://www.google.com/search?q=breathing+exercise\">" + messages[index] + "</a>";
+    message.href = "https://www.google.com/search?q=breathing+exercise";
   if (index === messages.indexOf("Learn how to make trance music."))
-    return "<a href=\"trancelessons.html\">" + messages[index] + "</a>";
+    message.href = "trancelessons.html";
   if (index === messages.indexOf("Learn how to make trance music.") + 1)
-    return "<a href=\"https://emergency.nofap.com/redirect?religious=true&cat=em\">" + messages[index] + "</a>";
-  if (index >= messages.indexOf("Watch anime."))
-    return "<a href=\"nextanime.html\">" + messages[index] + "</a>";
-  else if (index >= messages.indexOf("Walk around for about 1-2 minutes."))
-    return "Get your laptop off your lap, and stand up.<br>Change to appropriate clothes if necessary.<br>" + messages[index];
+    message.href = "https://emergency.nofap.com/redirect?religious=true&cat=em";
+  if (index >= messages.indexOf("Watch anime.") && index <= messages.lastIndexOf("Watch anime."))
+    message.href = "nextanime.html";
+  if (index >= messages.indexOf("Walk around for about 1-2 minutes."))
+    message.innerHTML = "Get your laptop off your lap, and stand up.<br>Change to appropriate clothes if necessary.<br>" + messages[index];
   else if (index >= messages.indexOf("Refill your bottle and drink water."))
-    return "Get your laptop off your lap, and stand up.<br>" + messages[index];
+    message.innerHTML = "Get your laptop off your lap, and stand up.<br>" + messages[index];
   else
-    return messages[index];
+    message.innerHTML = messages[index];
 }
 
 function randomMessage() {
@@ -125,49 +130,50 @@ function randomLink(message) {
 function randomTheme(number) {
   //Pick a random theme
   var theme;
+  var themeText = document.getElementById("theme");
   if (number !== 404) {
     theme = Math.floor(Math.random() * 5) + 1;
     switch (theme)
     {
       case 1:
-        document.getElementById("theme").innerHTML = "Brought to you by the moss green theme.";
+        themeText.innerHTML = "Brought to you by the moss green theme.";
         break;
       case 2:
-        document.getElementById("theme").innerHTML = "Brought to you by the blue theme.";
+        themeText.innerHTML = "Brought to you by the blue theme.";
         break;
       case 3:
-        document.getElementById("theme").innerHTML = "Brought to you by the violet theme.";
+        themeText.innerHTML = "Brought to you by the violet theme.";
         break;
       case 4:
-        document.getElementById("theme").innerHTML = "Brought to you by the sand theme.";
+        themeText.innerHTML = "Brought to you by the sand theme.";
         break;
       case 5:
-        document.getElementById("theme").innerHTML = "Brought to you by the gray theme.";
+        themeText.innerHTML = "Brought to you by the gray theme.";
         break;
       default:
-        document.getElementById("theme").innerHTML = "Brought to you by the black theme.";
+        themeText.innerHTML = "Brought to you by the black theme.";
     }
   } else {
     theme = Math.floor(Math.random() * 5) + 5;
     switch(theme)
     {
       case 5:
-        document.getElementById("theme").innerHTML = "Brought to you by the gray theme.";
+        themeText.innerHTML = "Brought to you by the gray theme.";
         break;
       case 6:
-        document.getElementById("theme").innerHTML = "Brought to you by the slate green theme.";
+        themeText.innerHTML = "Brought to you by the slate green theme.";
         break;
       case 7:
-        document.getElementById("theme").innerHTML = "Brought to you by the purple theme.";
+        themeText.innerHTML = "Brought to you by the purple theme.";
         break;
       case 8:
-        document.getElementById("theme").innerHTML = "Brought to you by the hot & dry theme.";
+        themeText.innerHTML = "Brought to you by the hot & dry theme.";
         break;
       case 9:
-        document.getElementById("theme").innerHTML = "Brought to you by the green theme.";
+        themeText.innerHTML = "Brought to you by the green theme.";
         break;
       default:
-        document.getElementById("theme").innerHTML = "Brought to you by the black theme.";
+        themeText.innerHTML = "Brought to you by the black theme.";
     }
   }
   document.body.className = "theme" + theme.toString();
@@ -198,8 +204,6 @@ function getTitle(id) {
 
 function loadAnime() {
   var anime = [];
-  anime.push(new Anime(getTitle("24") + "Episode 23","https://www.funimation.com/shows/pani-poni-dash/misfortunes-never-come-one-by-one"));
-  anime.push(new Anime(getTitle("145-2") + "Episode 11","https://vrv.co/watch/GY3VKMWZR/Saki:Hand-11-Threat"));
   anime.push(new Anime(getTitle("149") + "Episode 24","https://vrv.co/watch/GY4PXWX16/ReZERO-Starting-Life-in-Another-World-:The-Self-Proclaimed-Knight-and-the-Greatest-Knight"));
   anime.push(new Anime(getTitle("153") + "Episode 11","https://vrv.co/watch/GYJQVX336/Plastic-Memories:Rice-Omelette-Day"));
   anime.push(new Anime(getTitle("24") + "Episode 24","https://www.funimation.com/shows/pani-poni-dash/you-are-responsible-for-your-own-death"));
@@ -212,9 +216,16 @@ function loadAnime() {
   anime.push(new Anime(getTitle("153") + "Episode 13","https://vrv.co/watch/G6VN35ZPR/Plastic-Memories:I-Hope-One-Day-Youll-be-Reunited"));
   anime.push(new Anime(getTitle("35") + "Episode 1","https://vrv.co/watch/GR8VMM18R/GJ-CLUB:I-am-the-GJ-Club"));
   anime.push(new Anime(getTitle("35") + "Episode 2","https://vrv.co/watch/G6DQVVDGR/GJ-CLUB:Friendship-Love-Her-Abnormality"));
-  var x = document.getElementById("animelist");
+  var list = document.getElementById("animelist");
   for (var i in anime)
-    x.innerHTML = x.innerHTML + "<li><a href=\"" + anime[i].link + "\">" + anime[i].title + "</a></li>";
+  {
+    var y = document.createElement("li");
+    var z = document.createElement("a");
+    z.innerHTML = anime[i].title;
+    z.href = anime[i].link;
+    y.appendChild(z);
+    list.appendChild(y);
+  }
 }
 
 function getWidth() {
